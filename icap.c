59847d3225a878111c636953c350d14b817b7db9
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-24 15:37:01 +0100

    it works

--- icap.c
+++ icap.c
@@ -3,7 +3,7 @@
 
 #define Tmin 0.0
 #define Tmax 6.28
-#define Npts 10
+#define Npts 25
 
 int main(void){
     
@@ -16,7 +16,8 @@ int main(void){
     printf("Welcome to the Capacitor Current Calculator!\n");
     printf("\tEnter a value for capacitance in Farads: ");
     scanf(" %lf", &C);
-    printf("\tYou entered %.1lf", C);
+    //printf("\tYou entered %.1lf\n", C);
+    printf("\tHere are your results: \n\n");
     
     /*fill T[] array - time*/
     T[0] = Tmin;  
@@ -38,9 +39,11 @@ int main(void){
     for(i=0; i<Npts; i++){
         icap[i] = C*dVd[i];
     }
-    
-    printf("\tHere are your results: ");
 
-
+    printf("Ind\tC (F)\tT (s)\tv (v)\tdVdT\tic (A)\n");
+    printf("---\t-----\t-----\t-----\t-----\t-----\n");
+    for(i=0; i<Npts; i++){
+        printf("%d\t%.3lf\t%.3lf\t%.3lf\t%.3lf\t%.3lf\n", i, C, T[i], vsin[i], dVd[i], icap[i]);
+    }
     return 0;
 }
