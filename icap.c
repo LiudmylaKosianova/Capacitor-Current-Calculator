8dc6b9c3e7c03bfa5202e8b773de78e92d4d803e
Liudmyla Kosianova <Liudmyla.Kosianova@gmail.com>
2023-12-24 13:58:09 +0100

    print statements

--- icap.c
+++ icap.c
@@ -8,10 +8,17 @@
 int main(void){
     
     /*variables for this program*/
-    double Tstep = (Tmax - Tmin) / (Npts); 
+    double Tstep = (Tmax - Tmin) / (Npts-1); 
     double T[Npts], vsin[Npts], dVd[Npts], icap[Npts];
     double C;
     int i, j;
+
+    printf("Welcome to the Capacitor Current Calculator!\n");
+    printf("\tEnter a value for capacitance in Farads: ");
+    scanf(" %lf", &C);
+    printf("\tYou entered %.1lf", C);
+    /*fill T[] array*/
+    printf("\tHere are your results: ");
 
 
     return 0;
